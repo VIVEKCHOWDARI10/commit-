27bfed1febbb7bd85a529039fc08be5d634af5fe
pothuri vivek chowdari <chowdari@localhost.localdomain>
2025-10-27 09:39:15 +0530

    this is my first commit

---
+++ h.sh
@@ -0,0 +1,19 @@
+#! /bin/bash 
+
+f1=( "apple" 5 )
+echo ${#fruit[@]}
+f2=( "orange" "grapes" )
+f3=( "${f1[@]}" "${f2[@]}" )
+echo ${f3[@]}
+declare -A bike 
+bike=( [name]=spenlder [age]=20 [comp]=honda )
+echo ${bike[name]} ${bike[comp]}
+hi="hello"
+echo ${#hi}
+if [[ "${hi}" == *he* ]]
+then 
+	echo "it is not "
+else
+	echo "this is it "
+fi
+
