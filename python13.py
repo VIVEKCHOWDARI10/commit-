03adabc7e6abf46765be966497ced49fb1411603
pothuri vivek chowdari <chowdari@localhost.localdomain>
2025-10-27 15:13:16 +0530

    third commit

---
+++ python13.py
@@ -0,0 +1,20 @@
+import os
+
+def unnamed_pipe_demo():
+    r, w = os.pipe()  
+    pid = os.fork()   
+
+    if pid > 0:  
+        os.close(r)  
+        message = "Hello from parent"
+        os.write(w, message.encode())  
+        os.close(w)  
+
+    else:  
+        os.close(w)  
+        data = os.read(r, 100)  
+        print("Child received:", data.decode())
+        os.close(r)  
+
+unnamed_pipe_demo()
+
